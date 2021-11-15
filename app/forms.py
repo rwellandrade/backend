2ef3cda4d4a57297bcb5c611a45f1f7d75ab80a9
Rodrigo Wellington <rwellandrade@gmail.com>
2021-11-15 17:18:48 -0300

    (feat) making many to many relationship work, still needs to make quantity in this relationship to make things nice

--- app/forms.py
+++ app/forms.py
@@ -1,10 +1,26 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, DecimalField, IntegerField
+from wtforms import StringField, DecimalField, IntegerField, FormField, SelectField, FieldList
 from wtforms.validators import DataRequired
+# from wtforms_sqlalchemy import ModelFieldList
+from .models import Item, Product
+
+
+class ProductItemForm(FlaskForm):
+    its = SelectField('Item', coerce=int)
+    quantity = IntegerField('quantity')
+
+    def __init__(self, *args, **kwargs):
+        super(ProductItemForm, self).__init__(*args, **kwargs)
+        self.its.choices = [(item.id, item.name)
+                                 for item in Item.query.order_by(Item.name).all()]
+
+def available_items():
+    return Item.query.all()
 
 class ProductForm(FlaskForm):
     name = StringField('name', validators=[DataRequired()])
     price = DecimalField('price', validators=[DataRequired()])
+    its = FieldList(FormField(ProductItemForm), min_entries=3)
 
 
 class ItemForm(FlaskForm):
--- app/models.py
+++ app/models.py
@@ -1,15 +1,46 @@
 from .db import db
 from decimal import Decimal as D
 import sqlalchemy.types as types
+from sqlalchemy import Table, Column, Integer, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.associationproxy import association_proxy
+
+Base = declarative_base()
 
 class SqliteNumeric(types.TypeDecorator):
     impl = types.String
+
     def load_dialect_impl(self, dialect):
         return dialect.type_descriptor(types.VARCHAR(100))
+
     def process_bind_param(self, value, dialect):
         return str(value)
+
     def process_result_value(self, value, dialect):
         return D(value)
+
+
+product_items = db.Table('product_item',
+    db.Column('product_id', db.Integer, db.ForeignKey('product.id'), primary_key=True),
+    db.Column('item_id', db.Integer, db.ForeignKey('item.id'), primary_key=True),
+    db.Column('quantity', db.Integer, primary_key=True)
+)
+
+
+
+
+class Item(db.Model):
+    HEADERS = ['id', 'name', 'available_qty']
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    available_qty = db.Column(db.Integer, nullable=False)
+    # products = db.relationship(
+    #     "Product",
+    #     secondary=product_item_association_table, backref=db.backref('items'))
+
+    def __repr__(self):
+        return '<Item %r>' % self.name
+
 
 class Product(db.Model):
     HEADERS = ['id', 'name', 'image', 'price']
@@ -17,23 +48,19 @@ class Product(db.Model):
     name = db.Column(db.String(80), nullable=False)
     image = db.Column(db.Text, nullable=False)
     price = db.Column(SqliteNumeric(12, 2), nullable=False)
+    items = db.relationship(
+        "Item", secondary=product_items, lazy='subquery', backref=db.backref('products', lazy=True))
+
+    its = association_proxy('items', 'product_item', creator=lambda it: Item(product_id=it.product_id, quantity=it.quantity, item_id=it.item_id))
 
     def __repr__(self):
         return '<Produto %r>' % self.name
-
-class Item(db.Model):
-    HEADERS = ['id', 'name', 'available_qty']
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), nullable=False)
-    available_qty = db.Column(db.Integer, nullable=False)
-
-    def __repr__(self):
-        return '<Item %r>' % self.name
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=False)
+
 
 class Cart(db.Model):
     HEADERS = ['id', 'name', 'image', 'price']
@@ -42,7 +69,7 @@ class Cart(db.Model):
     creation_time = db.Column(db.Text, nullable=False)
     status = db.Column(SqliteNumeric(12, 2), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'),
-        nullable=False)
+                        nullable=False)
     # category = db.relationship('User',
     #     backref=db.backref('posts', lazy=True))
 
